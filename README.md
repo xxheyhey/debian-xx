c41f449e9974db6c2a76184cc3d7063e2c2d95e9
Vic <victenbokum@gmail.com>
2023-11-04 15:49:07 +0100

    even more finished?

--- README.md
+++ README.md
@@ -7,26 +7,30 @@ Look at install.sh to see what's going to be installed on your system.
 Make changes to the xrandr lines for your own system see [this](https://askubuntu.com/questions/377937/how-do-i-set-a-custom-resolution) for help.
 
 ### To install:
+- On a real machine:
 ```
 git clone https://github.com/xxheyhey/debian-xx.git
 cd debian-xx
 sudo ./install.sh
 ```
+- On a virtual machine:
+```
+git clone https://github.com/xxheyhey/debian-xx.git
+cd debian-xx
+sudo ./vminstall.sh
+```
+
 During the installation you will be prompted to install [auto-cpufreq](https://github.com/AdnanHodzic/auto-cpufreq). Follow the instructions that are given to install auto-cpufreq or not.
 
 Reboot after the installation is complete.
 
-### Manually do after (this is for myself):
+### Manually do after (mainly for myself):
 Bootstrap [packer.nvim](https://github.com/wbthomason/packer.nvim#bootstrapping)
+
+Check if there's a newer version of [R](https://cloud.r-project.org/src/base/R-4/) (currently installing R-4.3.2)
+
+Check [auto-cpufreq troubleshooting](https://github.com/AdnanHodzic/auto-cpufreq#troubleshooting) if experiencing problems with CPU performance
 
 Install [BLAST+](https://ftp.ncbi.nlm.nih.gov/blast/executables/LATEST/)
 
 Install [iqtree2](https://github.com/iqtree/iqtree2/blob/master/README.md)
-
-Copy my files (documents, pictures, etc.)
-
-Install LSP's through :Mason in Neovim
-
-Check [auto-cpufreq troubleshooting](https://github.com/AdnanHodzic/auto-cpufreq#troubleshooting) if experiencing problems with it
-
-Check if there's a newer version of [R](https://cloud.r-project.org/src/base/R-4/) (currently installing R-4.3.2)
---
+++ vminstall.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+username=$(id -u -n 1000)
+
+echo .gitconfig setup:
+echo What is your name? \(With capital first letter\)
+read name
+echo What is your email?
+read email
+echo What is your preferred editor? \(nano, nvim, vim, etc.\)
+read editor
+
+cat <<EOT >> .gitconfig
+[user]
+	name = $name
+	email = $email
+[core]
+	editor = $editor
+[pull]
+	rebase = false
+EOT
+mv .gitconfig /home/$username/
+
+
+cp .bashrc /home/$username/.bashrc
+cp .profile /home/$username/.profile
+cp .tmux* /home/$username/
+cp .x* /home/$username/
+cp .fehbg /home/$username/
+cp -R suckless/ /home/$username/
+cp -R Pictures/ /home/$username/
+cp -R .config/ /home/$username/
+cp -R .local/ /home/$username/
+
+ln -s $(which fdfind) /home/$username/.local/bin/fd
+
+apt update
+apt upgrade -y
+apt dist-upgrade -y
+apt autoremove -y
+apt autoclean -y
+
+apt install liblzma-dev rofi evince papirus-icon-theme xorg-dev gfortran libbz2-dev libpcre2-dev libreadline-dev libcurl4-openssl-dev bluetooth blueman network-manager network-manager-gnome xbindkeys vlc htop fonts-ubuntu alsa-utils thunar picom alacritty libxft-dev libxinerama-dev xorg git network-manager feh curl build-essential gettext unzip cmake xclip seqkit ninja-build python3 python3-dev python3-pip fzf fd-find pipx tmux -y
+
+pipx install cmake
+
+wget https://dl.thorium.rocks/debian/dists/stable/thorium.list
+mv thorium.list /etc/apt/sources.list.d/
+apt update
+apt install thorium-browser -y
+
+git clone https://github.com/neovim/neovim.git
+cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
+make install
+cd .. && rm -rf neovim
+
+apt update
+apt install -y ca-certificates curl gnupg
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key |  gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=21
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" |  tee /etc/apt/sources.list.d/nodesource.list
+apt update
+apt install nodejs -y
+
+wget https://packages.microsoft.com/config/debian/12/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+apt update
+apt install dotnet-sdk-7.0 -y
+
+
+wget https://cloud.r-project.org/src/base/R-4/R-4.3.2.tar.gz
+tar -xf R-4.3.2.tar.gz
+cd R-4.3.2/
+./configure
+make
+make install
+
+wget -qO - https://download.opensuse.org/repositories/home:/npreining:/debian-ubuntu-onedrive/Debian_12/Release.key | gpg --dearmor |  tee /usr/share/keyrings/obs-onedrive.gpg > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/obs-onedrive.gpg] https://download.opensuse.org/repositories/home:/npreining:/debian-ubuntu-onedrive/Debian_12/ ./" |  tee /etc/apt/sources.list.d/onedrive.list
+apt update
+apt install --no-install-recommends --no-install-suggests onedrive -y
+
+cd /home/$username/suckless/dmenu/ && make clean install
+cd /home/$username/suckless/dwm/ && make clean install
+cd /home/$username/suckless/slstatus/ && make clean install
+cd /home/$username/suckless/st/ && make clean install
+
+
+apt autoremove -y
+apt clean
+apt autoclean -y
