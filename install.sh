4275098143e31dc8ea0a7e0fbb48f31646048e9a
xxheyhey <95343562+xxheyhey@users.noreply.github.com>
2023-11-06 00:48:36 +0100

    fixes + removed auto-cpufreq

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ cat <<EOT >> .gitconfig
 EOT
 mv .gitconfig /home/$username/
 
-cp auto-cpufreq.conf /etc/
+
 cp .bashrc /home/$username/.bashrc
 cp .profile /home/$username/.profile
 cp .tmux* /home/$username/
@@ -31,7 +31,6 @@ cp -R .config/ /home/$username/
 cp -R .local/ /home/$username/
 cp -R .unison/ /home/$username/
 
-
 mkdir /home/$username/personal
 mkdir /home/$username/Desktop
 mkdir /home/$username/Downloads
@@ -42,7 +41,6 @@ mkdir /home/$username/mounts/wsl
 mkdir /lib/x86_64-linux-gnu/ntfs-3g
 cp ntfs-plugin-9000001a.so /lib/x86_64-linux-gnu/ntfs-3g/
 
-ln -s $(which fdfind) /home/$username/.local/bin/fd
 
 apt update
 apt upgrade -y
@@ -50,21 +48,13 @@ apt dist-upgrade -y
 apt autoremove -y
 apt autoclean -y
 
-apt install unison ntfs-3g thermald liblzma-dev rofi evince papirus-icon-theme xorg-dev gfortran libbz2-dev libpcre2-dev libreadline-dev libcurl4-openssl-dev bluetooth blueman network-manager network-manager-gnome xbindkeys vlc htop fonts-ubuntu alsa-utils thunar picom alacritty libxft-dev libxinerama-dev xorg git network-manager feh curl build-essential gettext unzip cmake xclip seqkit ninja-build python3 python3-dev python3-pip fzf fd-find pipx tmux -y
-
-pipx install cmake
+apt install xorg unison ntfs-3g thermald liblzma-dev rofi evince papirus-icon-theme gfortran libbz2-dev libpcre2-dev libreadline-dev libcurl4-openssl-dev bluetooth blueman network-manager network-manager-gnome xbindkeys vlc htop fonts-ubuntu alsa-utils thunar picom alacritty libxft-dev libxinerama-dev xorg-dev git network-manager feh curl build-essential gettext unzip cmake xclip seqkit ninja-build python3 python3-dev python3-pip fzf fd-find tmux -y
 
 wget https://dl.thorium.rocks/debian/dists/stable/thorium.list
 mv thorium.list /etc/apt/sources.list.d/
 apt update
 apt install thorium-browser -y
 
-git clone https://github.com/neovim/neovim.git
-cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
-make install
-cd .. && rm -rf neovim
-
-apt update
 apt install -y ca-certificates curl gnupg
 mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key |  gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
@@ -79,6 +69,21 @@ rm packages-microsoft-prod.deb
 apt update
 apt install dotnet-sdk-7.0 -y
 
+wget -qO - https://download.opensuse.org/repositories/home:/npreining:/debian-ubuntu-onedrive/Debian_12/Release.key | gpg --dearmor |  tee /usr/share/keyrings/obs-onedrive.gpg > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/obs-onedrive.gpg] https://download.opensuse.org/repositories/home:/npreining:/debian-ubuntu-onedrive/Debian_12/ ./" |  tee /etc/apt/sources.list.d/onedrive.list
+apt update
+apt install --no-install-recommends --no-install-suggests onedrive -y
+
+
+git clone https://github.com/neovim/neovim.git
+cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
+make install
+cd .. && rm -rf neovim
+
+cd /home/$username/suckless/dmenu/ && make clean install
+cd /home/$username/suckless/dwm/ && make clean install
+cd /home/$username/suckless/slstatus/ && make clean install
+cd /home/$username/suckless/st/ && make clean install
 
 wget https://cloud.r-project.org/src/base/R-4/R-4.3.2.tar.gz
 tar -xf R-4.3.2.tar.gz
@@ -86,24 +91,13 @@ cd R-4.3.2/
 ./configure
 make
 make install
+cd .. && rm -rf R-4.3.2*
 
-wget -qO - https://download.opensuse.org/repositories/home:/npreining:/debian-ubuntu-onedrive/Debian_12/Release.key | gpg --dearmor |  tee /usr/share/keyrings/obs-onedrive.gpg > /dev/null
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/obs-onedrive.gpg] https://download.opensuse.org/repositories/home:/npreining:/debian-ubuntu-onedrive/Debian_12/ ./" |  tee /etc/apt/sources.list.d/onedrive.list
-apt update
-apt install --no-install-recommends --no-install-suggests onedrive -y
+
+ln -s $(which fdfind) /home/$username/.local/bin/fd
 
 rm /etc/systemd/user/default.target.wants/onedrive.service
 
-cd /home/$username/suckless/dmenu/ && make clean install
-cd /home/$username/suckless/dwm/ && make clean install
-cd /home/$username/suckless/slstatus/ && make clean install
-cd /home/$username/suckless/st/ && make clean install
-
-
-git clone https://github.com/AdnanHodzic/auto-cpufreq.git
-cd auto-cpufreq &&  ./auto-cpufreq-installer
-cd .. && rm -rf auto-cpufreq
-auto-cpufreq --install
 
 apt autoremove -y
 apt clean
