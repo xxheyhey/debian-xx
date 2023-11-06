d017f6e7467888b3f074bed814e879ce21fec786
Vic <victenbokum@gmail.com>
2023-11-06 20:46:12 +0100

    ...

--- vminstall.sh
+++ vminstall.sh
@@ -50,7 +50,7 @@ apt autoremove -y
 apt autoclean -y
 
 # install packages
-apt install xorg lightdm r-base r-base-dev rofi evince papirus-icon-theme xbindkeys vlc htop fonts-ubuntu alsa-utils thunar picom alacritty libxft-dev libxinerama-dev git feh curl build-essential gettext unzip cmake xclip seqkit ninja-build python3 python3-dev python3-pip fzf fd-find tmux -y
+apt install xorg lightdm r-base r-base-dev rofi evince papirus-icon-theme xbindkeys vlc htop fonts-ubuntu alsa-utils thunar picom alacritty libxft-dev libxinerama-dev feh curl build-essential gettext unzip cmake xclip seqkit ninja-build python3 python3-dev python3-pip fzf fd-find tmux -y
 
 # install Thorium browser
 wget https://dl.thorium.rocks/debian/dists/stable/thorium.list
